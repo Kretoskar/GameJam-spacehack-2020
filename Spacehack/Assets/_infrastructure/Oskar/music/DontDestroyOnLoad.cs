8c1dae13943ca03dc4ae4888da3b944db7c3e37d
Oskar Kogut <kogutoskar123@gmail.com>
2020-11-07 14:39:19 +0100

    Add music player

---
+++ Spacehack/Assets/_infrastructure/Oskar/music/DontDestroyOnLoad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    private static DontDestroyOnLoad _instance ;
+ 
+    void Awake()
+    {
+        //if we don't have an [_instance] set yet
+        if(!_instance)
+            _instance = this ;
+        //otherwise, if we do, kill this thing
+        else
+            Destroy(this.gameObject) ;
+ 
+ 
+        DontDestroyOnLoad(this.gameObject) ;
+    }
+}
