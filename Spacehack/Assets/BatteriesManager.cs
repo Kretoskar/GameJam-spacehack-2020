44c1d829ac9c6896338485fd79287de770143b29
Oskar Kogut <kogutoskar123@gmail.com>
2020-11-07 08:45:02 +0100

    Add batteries logic

---
+++ Spacehack/Assets/BatteriesManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteriesManager : MonoBehaviour
+{
+    [SerializeField]
+    private int _maxWater = 500;
+
+    [SerializeField] private Material _noEmissionMaterial;
+
+    [SerializeField] private List<GameObject> _batteries;
+    
+    private WaterManager _waterManager;
+
+    private int _currentBatteriesCount = 5;
+    
+    private void Start()
+    {
+        _waterManager = FindObjectOfType<WaterManager>();
+        _waterManager.CurrentAmountChanged += UpdateBatteries;
+    }
+
+    private void UpdateBatteries(int amount)
+    {
+        //co ja robie kurwa
+        //chuj z tym szybko to napisalem to bedzie
+        if (amount < 400)
+            _batteries[0].GetComponent<MeshRenderer>().material = _noEmissionMaterial;
+            if (amount < 300)
+                _batteries[1].GetComponent<MeshRenderer>().material = _noEmissionMaterial;
+                if(amount < 200)
+                    _batteries[2].GetComponent<MeshRenderer>().material = _noEmissionMaterial;
+                    if(amount < 100)
+                        _batteries[3].GetComponent<MeshRenderer>().material = _noEmissionMaterial;
+                       if(amount < 5)
+                            _batteries[4].GetComponent<MeshRenderer>().material = _noEmissionMaterial;
+            
+    }
+}
--- Spacehack/Assets/WaterManager.cs
+++ Spacehack/Assets/WaterManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
@@ -6,13 +7,26 @@ using UnityEngine;
 public class WaterManager : MonoBehaviour
 {
     public int Amount;
-    public int CurrentAmount;
+
+    private int _currentAmount;
+    public int CurrentAmount
+    {
+        get => _currentAmount;
+        set
+        {
+            _currentAmount = value;
+            CurrentAmountChanged?.Invoke(_currentAmount);
+        }
+    }
+
     public int PerRow;
     public float Radius;
     public GameObject Prefab;
 
     [SerializeField] private WaterGenerator waterGenerator = default;
     [SerializeField] GameObject waterCollider = default;
+
+    public Action<int> CurrentAmountChanged;
     void Start()
     {
         CurrentAmount = Amount;
