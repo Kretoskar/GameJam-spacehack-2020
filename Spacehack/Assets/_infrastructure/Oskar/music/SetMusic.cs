8f20f1fa5b80df3df8f48be6ccc3c640c1c62989
Oskar Kogut <kogutoskar123@gmail.com>
2020-11-07 17:10:30 +0100

    Add gameplay music

---
+++ Spacehack/Assets/_infrastructure/Oskar/music/SetMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMusic : MonoBehaviour
+{
+   private AudioSource _audioSource;
+    [SerializeField] private AudioClip _clip;
+
+    private void Start()
+    {
+        _audioSource = FindObjectOfType<DontDestroyOnLoad>().GetComponent<AudioSource>();
+
+        if (_audioSource != null)
+        {
+            _audioSource.clip = _clip;
+            _audioSource.Play();
+        }
+    }
+}
