0cedfa5b20f51295ab4f58f4cd049f6e142839c1
Oskar Kogut <kogutoskar123@gmail.com>
2020-11-07 17:42:14 +0100

    Add base interaction class

---
+++ Spacehack/Assets/_infrastructure/Oskar/Interactions/CheckHasItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckHasItem : MonoBehaviour
+{
+    public bool UseItemIfHave(int itemID)
+    {
+        foreach (Transform child in transform)
+        {
+            var comp = child.GetComponent<InventoryItem>();
+            
+            if(comp == null) continue;
+
+            if (comp.Id == itemID)
+            {
+                Destroy(comp.gameObject);
+
+                StartCoroutine(DelayedCalculateWeight());
+                
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private IEnumerator DelayedCalculateWeight()
+    {
+        yield return new WaitForSeconds(.1f);
+        GetComponent<WeightCalculator>().CalculateWeight();
+    }
+}
---
+++ Spacehack/Assets/_infrastructure/Oskar/Interactions/Interaction.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField] private int _itemID;
+    [SerializeField] private CheckHasItem _hasItem;
+    
+    private bool _playerInRange;
+    private bool _interacted;
+    
+    private void Update()
+    {
+        if(_interacted) return;
+
+        if(_playerInRange)
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (_hasItem.UseItemIfHave(_itemID))
+                {
+                    _interacted = true;
+                    Interact();
+                }
+            }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            _playerInRange = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            _playerInRange = false;
+    }
+
+    protected virtual void Interact()
+    {
+        print("Interacting");
+    }
+}
