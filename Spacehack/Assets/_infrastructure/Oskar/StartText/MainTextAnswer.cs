bf47e8b94648bc59b63132e40832c18319716a4c
Oskar Kogut <kogutoskar123@gmail.com>
2020-11-07 14:08:59 +0100

    Add main menu scene

---
+++ Spacehack/Assets/_infrastructure/Oskar/StartText/MainTextAnswer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TypeWriterEffect))]
+public class MainTextAnswer : MonoBehaviour
+{
+    [SerializeField] private List<string> _answers;
+
+    private int _currentIndex = 0;
+    
+    private TypeWriterEffect _typeWriter;
+    
+    private void Start()
+    {
+        _typeWriter = GetComponent<TypeWriterEffect>();
+
+        _typeWriter.FullText = _answers[0];
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            GetText(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            GetText(false);
+        }
+    }
+
+    private void GetText(bool _goLeft)
+    {
+        if (_goLeft)
+        {
+            _currentIndex--;
+            
+            if (_currentIndex < 0)
+                _currentIndex = _answers.Count - 1;
+        }
+        else
+        {
+            _currentIndex++;
+
+            if (_currentIndex > _answers.Count - 1)
+                _currentIndex = 0;
+        }
+
+        _typeWriter.Rewrite(_answers[_currentIndex]);
+    }
+}
---
+++ Spacehack/Assets/_infrastructure/Oskar/StartText/TypeWriterEffect.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TypeWriterEffect : MonoBehaviour
+{
+    [SerializeField] private bool _initializeAtStart = true;
+    [SerializeField] private float _startDelay = 5;
+    [SerializeField] private float _delay = .1f;
+    
+    private string _fullText;
+
+    private TextMeshProUGUI _tmp;
+    
+    private string _currentText = "";
+
+    public string FullText
+    {
+        get => _fullText;
+        set
+        {
+            _currentText = "";
+            _fullText = value;
+        }
+    }
+
+    private void Start()
+    {
+        _tmp = GetComponent<TextMeshProUGUI>();
+
+        if (_initializeAtStart)
+            _fullText = _tmp.text;
+        
+        _tmp.text = _currentText;
+
+        StartCoroutine(WriteCoroutine(true));
+    }
+
+    public void Rewrite(string newText)
+    {
+        StopAllCoroutines();
+        StartCoroutine(RewriteCoroutine(newText));
+    }
+
+    private IEnumerator WriteCoroutine(bool waitForStart)
+    {
+        if(waitForStart)
+            yield return new WaitForSeconds(_startDelay);
+        
+        for (int i = 0; i < _fullText.Length; i++)
+        {
+            _currentText = _fullText.Substring(0, i);
+            _tmp.text = _currentText;
+            yield return new WaitForSeconds(_delay);
+        }
+
+        _delay = _delay / 5;
+    }
+
+    private IEnumerator RewriteCoroutine(string newText)
+    {
+        for (int i = _fullText.Length; i >= 0; i--)
+        {
+            _currentText = _fullText.Substring(0, i);
+            _tmp.text = _currentText;
+            yield return new WaitForSeconds(_delay);
+        }
+
+        FullText = newText;
+        
+        for (int i = 0; i < _fullText.Length; i++)
+        {
+            _currentText = _fullText.Substring(0, i);
+            _tmp.text = _currentText;
+            yield return new WaitForSeconds(_delay);
+        }
+    }
+}
