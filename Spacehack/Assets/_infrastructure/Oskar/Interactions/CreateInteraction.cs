2f2cd66cc78b9b2c45b982a146a1d35a57e8cd7f
Oskar Kogut <kogutoskar123@gmail.com>
2020-11-08 06:57:26 +0100

    Add building with iron

---
+++ Spacehack/Assets/_infrastructure/Oskar/Interactions/CreateInteraction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateInteraction : Interaction
+{
+    [SerializeField] private GameObject _createObject;
+    [SerializeField] private float _sizeUpTime;
+    
+    protected override void Interact()
+    {
+        base.Interact();
+        
+        _createObject.SetActive(true);
+
+        StartCoroutine(SizeUp());
+    }
+
+    private IEnumerator SizeUp()
+    {
+        float t = 0;
+
+        Vector3 startScale = _createObject.transform.localScale;
+        
+        while (t < 1)
+        {
+            t += Time.deltaTime / _sizeUpTime;
+            _createObject.transform.localScale = startScale * Mathf.Lerp(0, 1, t);
+            yield return null;
+        }
+    }
+}
